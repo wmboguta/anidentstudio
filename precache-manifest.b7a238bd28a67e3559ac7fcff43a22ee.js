5537b314f99bcd24332dada50818241e9cbb621d
Wojciech Boguta <wojciech.boguta@sollers.eu>
2023-11-12 21:26:58 +0100

    Build version

---
+++ precache-manifest.b7a238bd28a67e3559ac7fcff43a22ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50260fd9e232032aa05ac987fba93f08",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f56c8c90151d4c5aeadb",
+    "url": "./static/css/2.7e8848fa.chunk.css"
+  },
+  {
+    "revision": "174da10e2dfa3edc8858",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "f56c8c90151d4c5aeadb",
+    "url": "./static/js/2.dab5ecfc.chunk.js"
+  },
+  {
+    "revision": "174da10e2dfa3edc8858",
+    "url": "./static/js/main.9d1461aa.chunk.js"
+  },
+  {
+    "revision": "95ef95b2265544c17e41",
+    "url": "./static/js/runtime-main.5eba4e97.js"
+  },
+  {
+    "revision": "1a51dfbe5467607389d603c02a77cd10",
+    "url": "./static/media/bootstrap-icons.1a51dfbe.woff"
+  },
+  {
+    "revision": "ed62b9f1e0c75121f4d797a4a85730a2",
+    "url": "./static/media/bootstrap-icons.ed62b9f1.woff2"
+  },
+  {
+    "revision": "7e122787e9067f16e0f2bb071e72e2d4",
+    "url": "./static/media/doctor400x300.7e122787.svg"
+  },
+  {
+    "revision": "a40aed5472945c93ee8ad245be04a391",
+    "url": "./static/media/image1.a40aed54.jpg"
+  },
+  {
+    "revision": "938489160684bd485c55ea6369095702",
+    "url": "./static/media/katarzyna_boguta2.93848916.jpg"
+  },
+  {
+    "revision": "a2425c35452a78b7c20c74406ec3a881",
+    "url": "./static/media/logo.a2425c35.png"
+  }
+]);
